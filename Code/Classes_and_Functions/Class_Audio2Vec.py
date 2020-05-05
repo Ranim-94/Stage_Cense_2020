31ceeb41aab6e88eb11742a95fb8ec5e23baa029
Ranim Tom <ranim1tom@gmail.com>
2020-05-05 14:13:14 +0200

    Forward Pass using global max pooling for 1 sample final version

--- Code/Classes_and_Functions/Class_Audio2Vec.py
+++ Code/Classes_and_Functions/Class_Audio2Vec.py
@@ -6,16 +6,8 @@ import math
 from Classes_and_Functions.Helper_Functions import conv_block,dense_block
 
 
-
-
 '''
-The purpose of this module is to get rid of the conv_block
-
-and creating them using a loop, so we don't hard code them
-
-Also, the hidden layers size (number of filters in each layers) will also
-
-be inputted and not hard coded
+This class implement the Audio2Vec architecture
 '''
 
 class Audio2Vec(torch.nn.Module):
@@ -26,6 +18,11 @@ class Audio2Vec(torch.nn.Module):
       
         self.parameters_dictionary = parameters_dic
         
+        
+        self._CNN_layers_size = \
+        [parameters_dic['volume_input'],
+         *parameters_dic['list_filter_nb_per_layer']]
+        
         '''
         Creating the CNN hidden layers:
                in each layer of the CNN, self._CNN_layers_size
@@ -33,82 +30,81 @@ class Audio2Vec(torch.nn.Module):
                of the CNN part
         '''
         
-        
-        self._CNN_layers_size = \
-        [parameters_dic['volume_input'],
-         *parameters_dic['list_filter_nb_per_layer']]
-        
   
         conv_blokcs_list = []
         
+        '''
+        This loop will create the encoder architecture
+        '''
         
         for index, (in_f, out_f) in \
         enumerate(zip(self._CNN_layers_size,self._CNN_layers_size[1:])):
             
-             
             
             if index == len(parameters_dic['list_filter_nb_per_layer']) - 1:
                 
                 '''
                 - We have reached the last conv layer
                 
-                
                   In this layer, we do global max pooling instead of 
                 standard max pooling
+                    - so we set the pooling option to be False and
+                    create a CNN layer without pooling
+                    
+                    - The global max pooling will be done in the
+                    
+                    forward() method using torch.nn.functional.max_pool2d() 
                 
-                    - global and standard max pooling are the same
-                    
-                    - the only difference is that in global max pooling,
-                    the size of the pooling filter will have the same size
-                    as the input feature map
-                    
-                    - IN other words, the pooling filter height and width
-                    are the same as the output feature map 
-                    from the last conv layer
-                    
-                    - By this manner, we will obtain a single number
-                    and no need for reshaping operation since global max
-                    pooling applied to all the channles in the last conv
-                    layer will produce a vector
-                    
+                '''
 
-                '''
-                
-                
-                
-                '''
-                Changing the size of the pooling filter to make
-                it the same as the size of the last feature map
-                coming from the last CNN layer
-                
-                '''
-                 
                 parameters_dic['pooling_option'] = False
-                
-        
+
                 conv_blokcs_list.append(conv_block(in_f, out_f, parameters_dic))
                 
                 
             else:
                 
+                '''
+                In this block we are in the encoder part:
+                    - we create conv layers with standard max pooling layers also
+                '''
+                
                 conv_blokcs_list.append(conv_block(in_f, out_f, parameters_dic))
                 
 
-
         '''
-        --> Creating the CNN part:
-               
+        --> Creating the CNN part:      
                - since Sequential does not accept a list, 
                   we decompose the conv_blokcs by using the * operator.
-        '''
-       
-        self.CNN_part = torch.nn.Sequential(*conv_blokcs_list)
+        '''      
+        self.encoder = torch.nn.Sequential(*conv_blokcs_list)
        
  #---------------------- Linear Part ----------------------------------#
 
         '''
-        -->Dense layer part:
-               - we will do the same thing we did for the CNN part
+        --> Usually when reached the linear part we 
+        
+        do a flatten opeartion by computing the number of features
+        map produced in the last CNN layer and turing them into 1 D vector
+        
+        
+            - number of features in the last CNN layer  =
+            
+                number of filters used * heigth_feature_map * 
+                    width_feature_map
+                    
+        However in Audio2Vec the authors used global max pooling
+        
+        
+         - global and standard max pooling are the same
+                    
+         - the only difference is that in global max pooling,
+         the size of the pooling filter will have the same size
+         as the input feature map
+
+       --> So the first step is to set the dimension of the pooling filter
+         to be equal to the dimensions of the feature map produced by the 
+         last CNN layer
         '''
         
         self.parameters_dictionary['pooling_option'] = True
@@ -116,19 +112,29 @@ class Audio2Vec(torch.nn.Module):
 
         out_hidden_w,out_hidden_h = self.__compute_size()
         
-        print('-- Done Computing ! \n')
-        
+
+        '''
+        Setting the dimension of the pooling filter
+        '''
         self.parameters_dictionary['kernel_size_pool'] = \
-                (out_hidden_h,out_hidden_w * parameters_dic['stride_pool'])
+        (out_hidden_h,out_hidden_w * parameters_dic['stride_pool'])
+                
+        '''
+        This dimension will be used in the forward method
+        when calling torch.nn.functional.max_pool2d()
+        '''
         
         
-        # Step 1: Specifying the number of units in each dense layer
+        # Step 2: Specifying the number of units in each dense layer
         
         
         '''
         Since we have used global max pooling in the last CNN layer,
         
-        we don't need to do reshape anymore
+        the number of inputs in the last CNN layer will be the same
+        of the number of filters used since global max pooling produce 
+        
+        1 scalar number
        
         '''
         
@@ -148,44 +154,32 @@ class Audio2Vec(torch.nn.Module):
                        
         self.dense_part = torch.nn.Sequential(*dense_blokcs_list)
 
-        # # output layer
-        # self.last = torch.nn.Linear(self._dense_layers_size[-1], 
-        #                       parameters_dic['n_classes'])             
-       
-    
-        self.encoder = torch.nn.Sequential(self.CNN_part,self.dense_part)        
-       
        
         
     def forward(self, x):
  
         # Forward Propagation in all CNN layers   
-        x = self.CNN_part(x)
+        x = self.encoder(x)
         
-        print('--> Tensor shape after convolution:',x.shape,'\n')
 
         '''
         - Reaching the linear part:
-
+            - need to do global max pooling first
         '''
-        
-        print('--> Kernel Size for global max pooling:',
-              self.parameters_dictionary['kernel_size_pool'],'\n')
-        
+     
         x = torch.nn.functional.max_pool2d(x,kernel_size = 
             self.parameters_dictionary['kernel_size_pool'])
         
-        print('--> Tensor shape after global max pooling:',x.shape,'\n')
-        
+
+        '''
+        Transform the batch format into rank 2 tensor
+        '''
         x = x.reshape(-1, self._dense_layers_size[0])
 
         # Proessing in the dense layers part
         x = self.dense_part(x)
         
-        
     
-        
-        
         return x
  
        
@@ -194,6 +188,8 @@ class Audio2Vec(torch.nn.Module):
             '''
              - This method compute the size (width and height) of each 
              feature map outputted from each CNN Hidden layer
+                 - If there is pooling layer after convolution,
+                 also the dimension will be computed
            
               
             - We return the height and width of the last CNN layer
