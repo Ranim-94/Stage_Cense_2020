f895d453de46e7e5bec68276c1059ac63d475db7
Ranim Tom <ranim1tom@gmail.com>
2020-05-13 12:56:41 +0200

    Updating Mains: reorganizing the files, nothing important

--- Code/Main_Data_Building.py
+++ Code/Main_Data_Building.py
@@ -3,12 +3,15 @@
 
 import math
 
+import os
+
 import collections
 
 from Classes_and_Functions.Class_Dataset_Construction \
 import SpecCense_Construction
 
-
+from Classes_and_Functions.Class_Splitting_Dataset \
+import Splitting_Datasets
 
 
 '''
@@ -23,10 +26,10 @@ list containing sensor names
 
 
 
-# list_sensor_name = os.listdir('Data_Set/')
+list_sensor_name = os.listdir('Data_Set/')
 
 
-list_sensor_name = ['urn:osh:sensor:noisemonitoring:B8-27-EB-1A-0B-6C']
+
 
 '''
 Creating an ordered dictonary for the necessary paraemeters
@@ -39,7 +42,7 @@ in which will change during testing file existence
 od2 = collections.OrderedDict()
 
 
-od2['list_sensor_index'] = [v for v in range(0,1)]
+od2['list_sensor_index'] = [0]
 
 od2['year'] = [2019]
 
@@ -50,17 +53,17 @@ od2['days'] = [v for v in range(1,29)] # 1 --> 28
 
 width , margin  = int(math.pow(10,4)) , 250
 
-
-
 '''
 Casting into int because I use width as index in slicing
 '''
+
+
 
 saving_location_dict = {
     
     'Directory': 'CreatedDataset',
         
-    'File_Name':'train_spec_',
+    'File_Name_Spectrograms':'train_spec_',
     
     'File_Name_time_stamp':'train_time_',
     
@@ -68,21 +71,81 @@ saving_location_dict = {
     }
 
 
-# Creating the instance
-data_instance = SpecCense_Construction(ordered_dicton_parameters = 
-              od2, list_sensor_name = list_sensor_name,     
-               width = width, margin = margin, 
-              saving_location_dict = saving_location_dict,
-              option_remove = False)
+# # Creating the instance
+# data_instance = SpecCense_Construction(ordered_dicton_parameters = 
+#               od2, list_sensor_name = list_sensor_name,     
+#                width = width, margin = margin, 
+#               saving_location_dict = saving_location_dict,
+#               option_remove = False)
     
 
-# Calling the method
+# # Calling the method
 
-data_instance.creating_sample()
-
-
+# data_instance.creating_sample()
 
 
+
+
+
+
+
+
+# ---------------- Now here we splitt the total data into train, eval and valid--------
+
+
+'''
+Dictionary to specify the percentage
+of splitting
+'''
+splitting_parameters = {
+    
+    'training':0.7,
+    
+    'eval':0.2,
+    
+    'valid':0.1,
+    
+    }
+
+
+
+'''
+Choose directories names
+'''
+splitting_directories_names = {
+    
+    'train_directory_name':'Training_Set',
+    
+    'eval_directory_name':'Eval_Set',
+    
+    'valid_directory_name':'Validation_Set',
+    
+    }
+
+
+'''
+Instantiating
+'''
+
+splitt_instance = \
+Splitting_Datasets(saving_location_dict, splitting_parameters, 
+                                     splitting_directories_names)
+
+'''
+Calling the splitt method()
+'''
+
+splitt_instance.splitt()
+
+
+
+
+
+
+
+        
+        
+        
 
 
 
