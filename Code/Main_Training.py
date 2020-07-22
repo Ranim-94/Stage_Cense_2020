dac513e9f5c7a230cf1fe2c206ed130b1695fa3a
Ranim Tom <ranim1tom@gmail.com>
2020-07-22 14:37:59 +0200

    Update Main Training

--- Code/Main_Training.py
+++ Code/Main_Training.py
@@ -6,8 +6,10 @@ from math import pow
 
 from collections import OrderedDict
 
-from Classes_and_Functions.Class_Neural_Network_Training import \
-Neural_Network_Training
+
+
+from Classes_and_Functions.Class_Neural_Network_Training_Valid import \
+Neural_Network_Training_Valid
 
 from Classes_and_Functions.Class_Architecture import Model_Architecture
 
@@ -17,9 +19,9 @@ from Classes_and_Functions.Helper_Functions import plot_results
 
 # Specify which task to train
 
-task = { 'pretext_task': True,
+task = { 'pretext_task': False,
         
-        'sensor_classification': False
+        'sensor_classification': True
 
         }
 
@@ -68,7 +70,7 @@ model = Model_Architecture()
 if task['pretext_task'] == True:
     
 
-    coach = Neural_Network_Training(param.optimization_option,
+    coach = Neural_Network_Training_Valid(param.optimization_option,
                                     model.parameters_Audio2Vec,
                                     param.saving_location_dict,params_to_try,
                                     param.frame_width, param.rows_npy,
@@ -76,7 +78,7 @@ if task['pretext_task'] == True:
                                     start_from_iter,resume_training,
                                     loaded_model,mode = 'pretext_task')
 
-    valid_loss_per_epoch = coach.training()
+    _,accuracy_validation = coach.training()
     
     
     iter_nb = int(params_to_try['nb_of_iter'][0]) 
@@ -106,42 +108,42 @@ if task['pretext_task'] == True:
     
 elif task['sensor_classification'] == True:
     
-    # coach = Neural_Network_Training(param.optimization_option,
-    #             model.parameters_sensor_classification,
-    #             param.saving_location_dict,
-    #             params_to_try,
-    #             param.frame_width, param.rows_npy,
-    #             param.show_trace,param.model_names,save_point, 
-    #             start_from_iter,resume_training,
-    #             loaded_model,mode = 'sensor_classification')
+    coach = Neural_Network_Training_Valid(param.optimization_option,
+                model.parameters_sensor_classification,
+                param.saving_location_dict,
+                params_to_try,
+                param.frame_width, param.rows_npy,
+                param.show_trace,param.model_names,save_point, 
+                start_from_iter,resume_training,
+                loaded_model,mode = 'sensor_classification')
 
-    # coach.training()
-
-
-    iter_nb = int(params_to_try['nb_of_iter'][0])
-    
-    percentage = params_to_try['data_percentage'][0]
-
-    param_plot = {
-    
-    
-    'name_file': f"classif_no_emb_60000_6_Stable_Adam.pth",
-    
-    'title':f"Task: Sensor Classification Using Adam ",
-    
-    'xlabel':'Iteration Number',
-    
-    'ylabel':'Cross Entropy',
-    
-    'label':'Stable_Adam',
-    
-    'save':f"Classif_no_emb_{iter_nb}_{percentage}_Satble_Adam.pdf"
-    
-    
-    }
+    valid_loss_per_epoch,accuracy_validation = coach.training()
 
 
-    plot_results(param_plot,resume_training, start_from_iter ,iter_nb)
+    # iter_nb = int(params_to_try['nb_of_iter'][0])
+    
+    # percentage = params_to_try['data_percentage'][0]
+
+    # param_plot = {
+    
+    
+    # 'name_file': f"classif_no_emb_60000_6_Stable_Adam.pth",
+    
+    # 'title':f"Task: Sensor Classification Using Adam ",
+    
+    # 'xlabel':'Iteration Number',
+    
+    # 'ylabel':'Cross Entropy',
+    
+    # 'label':'Stable_Adam',
+    
+    # 'save':f"Classif_no_emb_{iter_nb}_{percentage}_Satble_Adam.pdf"
+    
+    
+    # }
+
+
+    # plot_results(param_plot,resume_training, start_from_iter ,iter_nb)
 
 
 
